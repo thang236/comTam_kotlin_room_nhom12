68b49579bb4450997d671380fffbb734888779ed
Louis hoàng <thang.uha236@gmail.com>
2024-06-03 15:35:39 +0700

    fix
    fix

--- app/src/main/java/com/example/comtam_kotlin_room/ui/screen/welcome/Welcome.kt
+++ app/src/main/java/com/example/comtam_kotlin_room/ui/screen/welcome/Welcome.kt
@@ -1,20 +1,27 @@
 package com.example.comtam_kotlin_room.ui.screen.welcome
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.comtam_kotlin_room.utils.Route
 
 @Composable
 fun WelcomeScreen(navController: NavHostController) {
+    Column(modifier = Modifier.padding(50.dp)) {
+        Text(text = "Welcome")
+        Button(onClick = {
+            navController.navigate(Route.MANAGER.screen)
+        }) {
+            Text(text = "Chuyển")
 
-    Text(text = "Welcome")
-    Button(onClick = {
-        navController.navigate(Route.MANAGER.screen)
-    }) {
-        Text(text = "Chuyển")
-
+        }
     }
+
+
 
 }
